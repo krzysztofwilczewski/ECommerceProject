23ef1f0c50958213ab7a29457ff4b1e7d41dd3f9
Wilczewski <krzysztof.wilczewski.pl@gmail.com>
2024-01-26 10:22:08 +0100

    Inital commit

---
+++ Shared/src/test/java/com/wilczewski/shared/SharedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilczewski.shared;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.wilczewski;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
