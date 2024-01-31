af313393b6bac82e90cbdd86b922a23e9ccd3607
Wilczewski <krzysztof.wilczewski.pl@gmail.com>
2024-01-31 19:45:03 +0100

    Admin home page

--- Common/Main/src/main/java/com/wilczewski/main/MainApplication.java
+++ Common/Main/src/main/java/com/wilczewski/main/MainApplication.java
@@ -2,8 +2,10 @@ package com.wilczewski.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"com.wilczewski.shared.entity"})
 public class MainApplication {
 
     public static void main(String[] args) {
---
+++ Shared/src/main/java/com/wilczewski/shared/entity/Product.java
@@ -0,0 +1,46 @@
+package com.wilczewski.shared.entity;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    public Product() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
