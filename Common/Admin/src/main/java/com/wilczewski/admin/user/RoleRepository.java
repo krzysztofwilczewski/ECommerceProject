121aa2d64cd936942e01e6b6726fcea36dbd5237
Wilczewski <krzysztof.wilczewski.pl@gmail.com>
2024-02-05 11:23:35 +0100

    Role repository

---
+++ Common/Admin/src/main/java/com/wilczewski/admin/user/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.wilczewski.admin.user;
+
+import com.wilczewski.shared.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+}
