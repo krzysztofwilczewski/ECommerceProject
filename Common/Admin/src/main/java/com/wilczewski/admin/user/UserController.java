7440dbf47f7076c02c28d158491a6baf7988d270
Wilczewski <krzysztof.wilczewski.pl@gmail.com>
2024-02-07 13:24:59 +0100

    Sorting for users

--- Common/Admin/src/main/java/com/wilczewski/admin/user/UserController.java
+++ Common/Admin/src/main/java/com/wilczewski/admin/user/UserController.java
@@ -4,6 +4,7 @@ import com.wilczewski.shared.entity.Role;
 import com.wilczewski.shared.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,12 +26,17 @@ public class UserController {
 
     @GetMapping("/users")
     public String listFirstPage(Model model){
-        return listByPage(1, model);
+        return listByPage(1, model, "firstName", "asc");
     }
 
     @GetMapping("/users/page/{pageNum}")
-    public String listByPage(@PathVariable(name = "pageNum") int pageNum, Model model) {
-        Page<User> page = userService.listByPage(pageNum);
+    public String listByPage(@PathVariable(name = "pageNum") int pageNum, Model model,
+                             @Param("sortField") String sortField, @Param("sortDir") String sortDir
+    ) {
+        System.out.println("Sort Field: " + sortField);
+        System.out.println("Sort Order: " + sortDir);
+
+        Page<User> page = userService.listByPage(pageNum, sortField, sortDir);
         List<User> listUsers = page.getContent();
 
         long startCount = (pageNum - 1) * UserService.USERS_PER_PAGE + 1;
@@ -39,12 +45,17 @@ public class UserController {
             endCount = page.getTotalElements();
         }
 
+        String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
+
         model.addAttribute("currentPage", pageNum);
         model.addAttribute("totalPages", page.getTotalPages());
         model.addAttribute("startCount", startCount);
         model.addAttribute("endCount", endCount);
         model.addAttribute("totalItems", page.getTotalElements());
         model.addAttribute("listUsers", listUsers);
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("reverseSortDir", reverseSortDir);
 
         return "users";
     }
--- Common/Admin/src/main/java/com/wilczewski/admin/user/UserService.java
+++ Common/Admin/src/main/java/com/wilczewski/admin/user/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -102,8 +103,12 @@ public class UserService {
         userRepository.updateStatus(id, enabled);
     }
 
-    public Page<User> listByPage(int pageNum) {
-        Pageable pageable = PageRequest.of(pageNum - 1, USERS_PER_PAGE);
+    public Page<User> listByPage(int pageNum, String sortField, String sortDir) {
+        Sort sort = Sort.by(sortField);
+
+        sort = sortDir.equals("asc") ? sort.ascending() : sort.descending();
+
+        Pageable pageable = PageRequest.of(pageNum - 1, USERS_PER_PAGE, sort);
         return userRepository.findAll(pageable);
     }
 
