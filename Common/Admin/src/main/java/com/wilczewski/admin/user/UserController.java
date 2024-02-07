c20590a2ea578149cdb6f1722a0a73d5a055b9f5
Wilczewski <krzysztof.wilczewski.pl@gmail.com>
2024-02-07 12:54:44 +0100

    Pagination for users

--- Common/Admin/src/main/java/com/wilczewski/admin/user/UserController.java
+++ Common/Admin/src/main/java/com/wilczewski/admin/user/UserController.java
@@ -3,6 +3,7 @@ package com.wilczewski.admin.user;
 import com.wilczewski.shared.entity.Role;
 import com.wilczewski.shared.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,8 +24,26 @@ public class UserController {
     }
 
     @GetMapping("/users")
-    public String listUsers(Model model){
-        List<User> listUsers = userService.listAll();
+    public String listFirstPage(Model model){
+        return listByPage(1, model);
+    }
+
+    @GetMapping("/users/page/{pageNum}")
+    public String listByPage(@PathVariable(name = "pageNum") int pageNum, Model model) {
+        Page<User> page = userService.listByPage(pageNum);
+        List<User> listUsers = page.getContent();
+
+        long startCount = (pageNum - 1) * UserService.USERS_PER_PAGE + 1;
+        long endCount = startCount + UserService.USERS_PER_PAGE - 1;
+        if (endCount > page.getTotalElements()) {
+            endCount = page.getTotalElements();
+        }
+
+        model.addAttribute("currentPage", pageNum);
+        model.addAttribute("totalPages", page.getTotalPages());
+        model.addAttribute("startCount", startCount);
+        model.addAttribute("endCount", endCount);
+        model.addAttribute("totalItems", page.getTotalElements());
         model.addAttribute("listUsers", listUsers);
 
         return "users";
--- Common/Admin/src/main/java/com/wilczewski/admin/user/UserService.java
+++ Common/Admin/src/main/java/com/wilczewski/admin/user/UserService.java
@@ -4,6 +4,9 @@ import com.wilczewski.shared.entity.Role;
 import com.wilczewski.shared.entity.User;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +16,8 @@ import java.util.NoSuchElementException;
 @Service
 @Transactional
 public class UserService {
+
+    public static final int USERS_PER_PAGE = 4;
 
     private UserRepository userRepository;
 
@@ -95,6 +100,11 @@ public class UserService {
 
     public void updateUserStatus(Integer id, boolean enabled){
         userRepository.updateStatus(id, enabled);
+    }
+
+    public Page<User> listByPage(int pageNum) {
+        Pageable pageable = PageRequest.of(pageNum - 1, USERS_PER_PAGE);
+        return userRepository.findAll(pageable);
     }
 
 }
