751c6b9b596c464095cc250093da25b602af0b9a
Wilczewski <krzysztof.wilczewski.pl@gmail.com>
2024-02-22 13:04:53 +0100

    Sort, filter and pagination for brands

--- Common/Admin/src/main/java/com/wilczewski/admin/brand/BrandRepository.java
+++ Common/Admin/src/main/java/com/wilczewski/admin/brand/BrandRepository.java
@@ -1,7 +1,10 @@
 package com.wilczewski.admin.brand;
 
 import com.wilczewski.shared.entity.Brand;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface BrandRepository extends JpaRepository<Brand, Integer>, PagingAndSortingRepository<Brand, Integer> {
@@ -9,4 +12,7 @@ public interface BrandRepository extends JpaRepository<Brand, Integer>, PagingAn
     public Long countById(Integer id);
 
     public Brand findByName(String name);
+
+    @Query("SELECT b FROM Brand b WHERE b.name LIKE %?1%")
+    public Page<Brand> findAll(String keyword, Pageable pageable);
 }
--- Common/Admin/src/main/java/com/wilczewski/admin/brand/BrandService.java
+++ Common/Admin/src/main/java/com/wilczewski/admin/brand/BrandService.java
@@ -2,6 +2,10 @@ package com.wilczewski.admin.brand;
 
 import com.wilczewski.shared.entity.Brand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,6 +13,8 @@ import java.util.NoSuchElementException;
 
 @Service
 public class BrandService {
+
+    public static final int BRANDS_PER_PAGE = 10;
 
     private BrandRepository brandRepository;
 
@@ -56,5 +62,19 @@ public class BrandService {
         }
 
         return "OK";
+    }
+
+    public Page<Brand> listByPage(int pageNum, String sortField, String sortDir, String keyword) {
+        Sort sort = Sort.by(sortField);
+
+        sort = sortDir.equals("asc") ? sort.ascending() : sort.descending();
+
+        Pageable pageable = PageRequest.of(pageNum - 1, BRANDS_PER_PAGE, sort);
+
+        if (keyword != null) {
+            return brandRepository.findAll(keyword, pageable);
+        }
+
+        return brandRepository.findAll(pageable);
     }
 }
