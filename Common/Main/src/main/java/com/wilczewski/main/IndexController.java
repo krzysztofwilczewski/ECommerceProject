622fa88c4e9008c1d5fccc40f53bb392977a8343
Wilczewski <krzysztof.wilczewski.pl@gmail.com>
2024-03-17 20:44:53 +0100

    Home page - listing categories

--- Common/Main/src/main/java/com/wilczewski/main/IndexController.java
+++ Common/Main/src/main/java/com/wilczewski/main/IndexController.java
@@ -1,13 +1,30 @@
 package com.wilczewski.main;
 
+import com.wilczewski.main.category.CategoryService;
+import com.wilczewski.shared.entity.Category;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class IndexController {
 
+    private CategoryService categoryService;
+
+    @Autowired
+    public IndexController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
     @GetMapping("")
-    public String main(){
+    public String main(Model model){
+
+        List<Category> listCategories = categoryService.listNoChildrenCategories();
+        model.addAttribute("listCategories", listCategories);
+
         return "index";
     }
 }
---
+++ Common/Main/src/main/java/com/wilczewski/main/category/CategoryService.java
@@ -0,0 +1,36 @@
+package com.wilczewski.main.category;
+
+import com.wilczewski.shared.entity.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Service
+public class CategoryService {
+
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> listNoChildrenCategories(){
+
+        List<Category> listNoChildrenCategories = new ArrayList<>();
+
+        List<Category> listEnabledCategories = categoryRepository.findAllEnabled();
+
+        listEnabledCategories.forEach(category -> {
+            Set<Category> children = category.getChildren();
+            if (children == null || children.size() == 0) {
+             listNoChildrenCategories.add(category);
+            }
+        });
+        return listNoChildrenCategories;
+    }
+
+}
